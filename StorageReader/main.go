34e089bdf8c7fba0f8811a4674d3f832a6b74ab6
jake <bigjake2s@gmail.com>
2013-07-24 11:17:48 -0400

    Adding StorageReader service

---
+++ StorageReader/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"git.300brand.com/coverage"
+	"git.300brand.com/coverage/config"
+	"git.300brand.com/coverage/skytypes"
+	"git.300brand.com/coverage/storage/mongo"
+	"github.com/skynetservices/skynet"
+	"github.com/skynetservices/skynet/service"
+	"log"
+)
+
+type Service struct{}
+
+const ServiceName = "StorageReader"
+
+var (
+	_ service.ServiceDelegate = &Service{}
+	m *mongo.Mongo
+)
+
+// Funcs required for ServiceDelegate
+
+func (s *Service) MethodCalled(m string) {}
+
+func (s *Service) MethodCompleted(m string, d int64, err error) {}
+
+func (s *Service) Registered(service *service.Service) {}
+
+func (s *Service) Started(service *service.Service) {
+	host, db := config.Mongo.Host, config.Mongo.Database
+	log.Printf("Connecting to MongoDB %s %s", host, db)
+	m = mongo.New(host, db)
+	if err := m.Connect(); err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %s", err)
+	}
+	log.Println("Connected to MongoDB")
+}
+
+func (s *Service) Stopped(service *service.Service) {
+	log.Println("Closing MongoDB connection")
+	m.Close()
+}
+
+func (s *Service) Unregistered(service *service.Service) {}
+
+// Service funcs
+
+func (s *Service) GetFeed(ri *skynet.RequestInfo, in *skytypes.ObjectId, out *coverage.Feed) (err error) {
+	return m.GetFeed(in.Id, out)
+}
+
+func (s *Service) OldestFeed(ri *skynet.RequestInfo, in *skytypes.ObjectIds, out *coverage.Feed) (err error) {
+	return m.GetOldestFeed(in.Ids, out)
+}
+
+// Main
+
+func main() {
+	log.SetFlags(log.Lshortfile | log.Lmicroseconds)
+	log.SetPrefix(ServiceName + " ")
+
+	sc, _ := skynet.GetServiceConfig()
+	sc.Name = ServiceName
+	sc.Region = "Storage"
+	sc.Version = "1"
+
+	s := service.CreateService(&Service{}, sc)
+	defer s.Shutdown()
+
+	s.Start(true).Wait()
+}
