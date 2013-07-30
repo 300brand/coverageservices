42410334ca2f2423e53c47b8c7ec7f4e74df63f1
jake <bigjake2s@gmail.com>
2013-07-30 15:56:48 -0400

    Added notification support after search finalization

--- Search/main.go
+++ Search/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/skynetservices/skynet/client"
 	"github.com/skynetservices/skynet/service"
 	"log"
+	"sync"
 	"time"
 )
 
@@ -17,6 +18,7 @@ const ServiceName = "Search"
 
 var (
 	_             service.ServiceDelegate = &Service{}
+	Search        *client.ServiceClient
 	StorageReader *client.ServiceClient
 	StorageWriter *client.ServiceClient
 )
@@ -31,6 +33,12 @@ func (s *Service) Stopped(service *service.Service)             {}
 func (s *Service) Unregistered(service *service.Service)        {}
 
 // Service funcs
+
+func (s *Service) NotifyComplete(ri *skynet.RequestInfo, in *skytypes.SearchQuery, out *skytypes.NullType) (err error) {
+	// TODO Gather full articles and send to the notification URL
+	log.Printf("Sending notifacation to %s", in.Notify)
+	return
+}
 
 func (s *Service) Search(ri *skynet.RequestInfo, in *skytypes.SearchQuery, out *skytypes.SearchQueryResponse) (err error) {
 	// Validation
@@ -60,9 +68,21 @@ func (s *Service) Search(ri *skynet.RequestInfo, in *skytypes.SearchQuery, out *
 		Id:    cs.Id,
 		Query: cs.Q,
 	}
+	var wg sync.WaitGroup
 	for _, ds.Date = range dates {
-		go StorageWriter.SendOnce(ri, "DateSearch", ds, skytypes.Null)
+		wg.Add(1)
+		go func() {
+			StorageWriter.SendOnce(ri, "DateSearch", ds, skytypes.Null)
+			wg.Done()
+		}()
 	}
+
+	// Wait for all of the DateSearch calls to finish, then send the
+	// notification of completeness
+	go func(ri skynet.RequestInfo, q skytypes.SearchQuery) {
+		wg.Wait()
+		Search.SendOnce(&ri, "NotifyComplete", q, skytypes.Null)
+	}(*ri, *in)
 
 	// Prepare information for the caller
 	out.Id = cs.Id
@@ -80,6 +100,7 @@ func main() {
 	cc, _ := skynet.GetClientConfig()
 	c := client.NewClient(cc)
 
+	Search = c.GetService("Search", "", "", "")
 	StorageReader = c.GetService("StorageReader", "", "", "")
 	StorageWriter = c.GetService("StorageWriter", "", "", "")
 
