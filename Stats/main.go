2bdb5d9545543cd2ac643ddccefe0dd9da3a505c
jake <bigjake2s@gmail.com>
2013-08-27 11:06:04 -0400

    Filled in Stats Completed call for tracking memory and call counts

--- Stats/main.go
+++ Stats/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"git.300brand.com/coverage/config"
 	"git.300brand.com/coverage/skytypes"
 	"github.com/cyberdelia/statsd"
 	"github.com/skynetservices/skynet"
 	"github.com/skynetservices/skynet/service"
 	"log"
+	"strings"
+	"time"
 )
 
 type Service struct{}
@@ -56,8 +59,35 @@ func (s *Service) Increment(ri *skynet.RequestInfo, stat *skytypes.Stat, out *sk
 	return
 }
 
-func (s *Service) MethodCall(ri *skynet.RequestInfo, stat *skytypes.Stat, out *skytypes.NullType) (err error) {
+func (s *Service) Completed(ri *skynet.RequestInfo, stat *skytypes.Stat, out *skytypes.NullType) (err error) {
+	log.Printf("Stat Info: %v", stat)
+	rate := float64(1)
+	base := statJoin(statBase(stat), "MethodCompleted")
+	client.Increment(statJoin(base, "count"), 1, rate)
+	client.Timing(statJoin(base, "duration"), int(time.Duration(stat.Nanos)/time.Millisecond), rate)
+	if stat.Error != nil {
+		client.Increment(statJoin(base, "errors"), 1, rate)
+	}
+	client.Gauge(statJoin(base, "alloc"), int(stat.Mem.Alloc), rate)
+	client.Gauge(statJoin(base, "totalAlloc"), int(stat.Mem.TotalAlloc), rate)
+	client.Gauge(statJoin(base, "mallocs"), int(stat.Mem.Mallocs), rate)
+	client.Gauge(statJoin(base, "heapAlloc"), int(stat.Mem.HeapAlloc), rate)
+	client.Gauge(statJoin(base, "heapSys"), int(stat.Mem.HeapSys), rate)
+	client.Gauge(statJoin(base, "heapInuse"), int(stat.Mem.HeapInuse), rate)
+	client.Gauge(statJoin(base, "stackSys"), int(stat.Mem.StackSys), rate)
+	client.Gauge(statJoin(base, "stackInuse"), int(stat.Mem.StackInuse), rate)
+	client.Gauge(statJoin(base, "numGC"), int(stat.Mem.NumGC), rate)
 	return
+}
+
+// Support funcs
+
+func statBase(stat *skytypes.Stat) string {
+	return fmt.Sprintf("%s.%s.%s.%s", stat.Config.Name, stat.Name, stat.Config.Version, stat.Config.Region)
+}
+
+func statJoin(paths ...string) string {
+	return strings.Join(paths, ".")
 }
 
 // Main
