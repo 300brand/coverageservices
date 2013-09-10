ff15205d657325873a5f1101a704e09e10538887
jake <bigjake2s@gmail.com>
2013-09-10 14:59:07 -0400

    Adding the Social service

---
+++ Social/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"git.300brand.com/coverage"
+	"git.300brand.com/coverage/social"
+	"git.300brand.com/coverageservices/skynetstats"
+	"github.com/skynetservices/skynet"
+	"github.com/skynetservices/skynet/client"
+	"github.com/skynetservices/skynet/service"
+	"log"
+)
+
+type Service struct {
+	Config *skynet.ServiceConfig
+}
+
+const ServiceName = "Social"
+
+var (
+	_     service.ServiceDelegate = &Service{}
+	Stats *client.ServiceClient
+)
+
+// Funcs required for ServiceDelegate
+
+func (s *Service) MethodCalled(m string) {}
+
+func (s *Service) MethodCompleted(m string, d int64, err error) {
+	skynetstats.Completed(m, d, err)
+}
+
+func (s *Service) Registered(service *service.Service) {
+	skynetstats.Start(s.Config, Stats)
+}
+
+func (s *Service) Started(service *service.Service) {}
+
+func (s *Service) Stopped(service *service.Service) {}
+
+func (s *Service) Unregistered(service *service.Service) {
+	skynetstats.Stop()
+}
+
+// Service funcs
+
+func (s *Service) Article(ri *skynet.RequestInfo, in *coverage.Article, out *social.Stats) (err error) {
+	*out, err = social.Fetch(in.URL)
+	return
+}
+
+// Main
+
+func main() {
+	log.SetFlags(log.Lshortfile | log.Lmicroseconds)
+	log.SetPrefix(ServiceName + " ")
+
+	cc, _ := skynet.GetClientConfig()
+	c := client.NewClient(cc)
+
+	Stats = c.GetService("Stats", "", "", "")
+
+	sc, _ := skynet.GetServiceConfig()
+	sc.Name = ServiceName
+	sc.Region = "Social"
+	sc.Version = "1"
+
+	s := service.CreateService(&Service{sc}, sc)
+	defer s.Shutdown()
+
+	s.Start(true).Wait()
+}
