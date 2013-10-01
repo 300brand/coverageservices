237c9f5800766eac3150a525f0dd0037f4291e53
jake <bigjake2s@gmail.com>
2013-10-01 15:51:15 -0400

    Finally committing redeploy script

---
+++ redeploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+export SKYNET_DZHOST=192.168.20.20:8046
+
+# sky stop -service=WebAPI
+# sky stop -service=Social
+# sky stop -service=Search
+# sky deploy git.300brand.com/coverageservices/WebAPI -host=192.168.20.20
+# sky deploy git.300brand.com/coverageservices/Social -host=192.168.20.20
+# sky deploy git.300brand.com/coverageservices/Search -host=192.168.20.20
+# exit
+
+go install -v git.300brand.com/coverageservices/{Stats,Article,Feed,Search,Social,StorageReader,StorageWriter,Manager,Queue,WebAPI} || exit
+
+sky stop
+sleep 1
+# Deploy to all:
+for S in Stats Article Feed Search StorageReader StorageWriter Social; do
+	sky deploy git.300brand.com/coverageservices/$S
+done
+# Deploy to sable:
+for S in Manager Queue WebAPI; do
+#for S in Manager Queue WebAPI Article Feed Search StorageReader StorageWriter; do
+	sky deploy git.300brand.com/coverageservices/$S -host=192.168.20.20
+done
+exit
+
+#sky stop -service=Search
+#sky stop -service=StorageReader
+#sky stop -service=StorageWriter
+#sky stop -service=WebAPI
+#sky deploy git.300brand.com/coverageservices/StorageReader -host=192.168.20.20
+#sky deploy git.300brand.com/coverageservices/StorageWriter -host=192.168.20.20
+#sky deploy git.300brand.com/coverageservices/Search -host=192.168.20.20
+#sky deploy git.300brand.com/coverageservices/WebAPI -host=192.168.20.20
