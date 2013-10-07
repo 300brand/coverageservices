1570b64d04ee1e51971d4a0cafa6e555a59d3e01
jake <bigjake2s@gmail.com>
2013-10-07 12:55:08 -0400

    Removed a whole bunch of config munging per latest updates to TOML config

--- service/service.go
+++ service/service.go
@@ -1,8 +1,6 @@
 package service
 
 import (
-	"flag"
-	"fmt"
 	"github.com/jbaikge/disgo"
 	"github.com/jbaikge/go-toml-config"
 )
@@ -13,35 +11,15 @@ type Service interface {
 	Start(client *disgo.Client) error
 }
 
-type enableDisable string
-
 type serviceInfo struct {
 	Service       Service
-	FlagEnabled   *enableDisable
 	ConfigEnabled *bool
 }
 
 var services = make(map[string]serviceInfo)
 
-var _ flag.Value = new(enableDisable)
-
-func (ed *enableDisable) Set(v string) (err error) {
-	switch v {
-	case "enable", "disable", "":
-		*ed = enableDisable(v)
-	default:
-		return fmt.Errorf("Invalid value \"%s\"", v)
-	}
-	return
-}
-
-func (ed *enableDisable) String() string { return string(*ed) }
-
 func (si serviceInfo) Enabled() bool {
-	if si.FlagEnabled.String() == "disable" {
-		return false
-	}
-	return si.FlagEnabled.String() == "enable" || *si.ConfigEnabled
+	return *si.ConfigEnabled
 }
 
 func Register(name string, service Service) {
@@ -52,14 +30,9 @@ func Register(name string, service Service) {
 		panic("service: Register called twice for " + name)
 	}
 
-	// Set up flag
-	fe := new(enableDisable)
-	//flag.Var(fe, name, "Valid values: \"enable\", \"disable\", or \"\" (uses value in config file)")
-
 	// Add service
 	services[name] = serviceInfo{
 		Service:       service,
-		FlagEnabled:   fe,
 		ConfigEnabled: config.Bool(name+".enabled", false),
 	}
 
