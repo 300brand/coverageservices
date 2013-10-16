d7eea8637d945a9cbca9a649dadba069e8f2282f
jake <bigjake2s@gmail.com>
2013-10-16 11:08:41 -0400

    Fixes for dumb errors after removing go-func

--- Article/main.go
+++ Article/main.go
@@ -35,52 +35,52 @@ func (s *Service) Start(client *disgo.Client) (err error) {
 func (s *Service) Process(in *coverage.Article, out *disgo.NullType) (err error) {
 	start := time.Now()
 
-	hostChunks := strings.Split(a.URL.Host, ".")
+	hostChunks := strings.Split(in.URL.Host, ".")
 	if len(hostChunks) > 2 {
 		hostChunks = hostChunks[len(hostChunks)-2:]
 	}
 	domain := strings.Join(hostChunks, "_")
 
 	// Download article
-	if err := downloader.Article(a); err != nil {
+	if err = downloader.Article(in); err != nil {
 		logger.Debug.Printf("%s %s.%s", time.Since(start), "Process.Download.Failure", domain)
-		logger.Error.Printf("%s[%s] Error downloading: %s", a.ID.Hex(), a.URL, err)
+		logger.Error.Printf("%s[%s] Error downloading: %s", in.ID.Hex(), in.URL, err)
 		return
 	}
 	logger.Debug.Printf("%s %s.%s", time.Since(start), "Process.Download.Success", domain)
-	logger.Debug.Printf("%d %s.%s", len(a.Text.HTML), "Process.Bandwidth", domain)
+	logger.Debug.Printf("%d %s.%s", len(in.Text.HTML), "Process.Bandwidth", domain)
 
 	// If any step fails along the way, save the article's state
 	defer func() {
-		if err := s.client.Call("StorageWriter.Article", a, a); err != nil {
-			logger.Error.Printf("%s[%s] Error saving: %s", a.ID.Hex(), a.URL, err)
+		if err = s.client.Call("StorageWriter.Article", in, in); err != nil {
+			logger.Error.Printf("%s[%s] Error saving: %s", in.ID.Hex(), in.URL, err)
 			return
 		}
 		inc := types.Inc{
-			Id:    a.PublicationId,
+			Id:    in.PublicationId,
 			Delta: 1,
 		}
-		if err := s.client.Call("StorageWriter.PubIncArticles", inc, disgo.Null); err != nil {
-			logger.Error.Printf("%s[%s] Error incrementing article count for pub [%s]: %s", a.ID.Hex(), a.URL, a.PublicationId.Hex(), err)
+		if err = s.client.Call("StorageWriter.PubIncArticles", inc, disgo.Null); err != nil {
+			logger.Error.Printf("%s[%s] Error incrementing article count for pub [%s]: %s", in.ID.Hex(), in.URL, in.PublicationId.Hex(), err)
 		}
 	}()
 
 	// Extract body
 	start = time.Now()
-	if err := body.SetBody(a); err != nil || a.Text.Body.Text == nil || len(a.Text.Body.Text) == 0 {
+	if err = body.SetBody(in); err != nil || in.Text.Body.Text == nil || len(in.Text.Body.Text) == 0 {
 		logger.Debug.Printf("%s %s.%s", time.Since(start), "Process.Body.Failure", domain)
-		logger.Error.Printf("%s[%s] Error setting body: %s", a.ID.Hex(), a.URL, err)
+		logger.Error.Printf("%s[%s] Error setting body: %s", in.ID.Hex(), in.URL, err)
 		return
 	}
 	logger.Debug.Printf("%s %s.%s", time.Since(start), "Process.Body.Success", domain)
-	logger.Debug.Printf("%d %s.%s", len(a.Text.Body.Text), "Process.BodyLength", domain)
+	logger.Debug.Printf("%d %s.%s", len(in.Text.Body.Text), "Process.BodyLength", domain)
 
 	// Filter out individual words
-	a.Text.Words.All = lexer.Words(a.Text.Body.Text)
-	logger.Debug.Printf("%d %s", len(a.Text.Words.All), "Process.Words")
+	in.Text.Words.All = lexer.Words(in.Text.Body.Text)
+	logger.Debug.Printf("%d %s", len(in.Text.Words.All), "Process.Words")
 
 	// Filter out Keywords
-	a.Text.Words.Keywords = lexer.Keywords(a.Text.Body.Text)
-	logger.Debug.Printf("%d %s", len(a.Text.Words.Keywords), "Process.Keywords")
+	in.Text.Words.Keywords = lexer.Keywords(in.Text.Body.Text)
+	logger.Debug.Printf("%d %s", len(in.Text.Words.Keywords), "Process.Keywords")
 	return
 }
