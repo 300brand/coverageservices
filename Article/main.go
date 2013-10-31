1f859ade265ade32e461b0021b240e840510d2fb
jake <bigjake2s@gmail.com>
2013-10-31 12:17:08 -0400

    Improved Article service logging; Pushing an error when the downloaded file = downloader.MaxFileSize

--- Article/main.go
+++ Article/main.go
@@ -1,6 +1,7 @@
 package Article
 
 import (
+	"fmt"
 	"github.com/300brand/coverage"
 	"github.com/300brand/coverage/article/body"
 	"github.com/300brand/coverage/article/lexer"
@@ -8,7 +9,6 @@ import (
 	"github.com/300brand/coverageservices/service"
 	"github.com/300brand/disgo"
 	"github.com/300brand/logger"
-	"strings"
 	"time"
 )
 
@@ -33,45 +33,46 @@ func (s *Service) Start(client *disgo.Client) (err error) {
 
 func (s *Service) Process(in *coverage.Article, out *disgo.NullType) (err error) {
 	start := time.Now()
+	prefix := fmt.Sprintf("Article.Process: [P:%s] [F:%s] [A:%s] [U:%s]", in.PublicationId.Hex(), in.FeedId.Hex(), in.ID.Hex(), in.URL)
 
-	hostChunks := strings.Split(in.URL.Host, ".")
-	if len(hostChunks) > 2 {
-		hostChunks = hostChunks[len(hostChunks)-2:]
-	}
-	domain := strings.Join(hostChunks, "_")
+	// Used for stats - coming soon
+	// hostChunks := strings.Split(in.URL.Host, ".")
+	// if len(hostChunks) > 2 {
+	// 	hostChunks = hostChunks[len(hostChunks)-2:]
+	// }
+	// domain := strings.Join(hostChunks, "_")
 
 	// Download article
 	if err = downloader.Article(in); err != nil {
-		logger.Debug.Printf("%s %s.%s", time.Since(start), "Process.Download.Failure", domain)
-		logger.Error.Printf("%s[%s] Error downloading: %s", in.ID.Hex(), in.URL, err)
+		logger.Error.Printf("%s Download error: %s", prefix, err)
 		return
 	}
-	logger.Debug.Printf("%s %s.%s", time.Since(start), "Process.Download.Success", domain)
-	logger.Debug.Printf("%d %s.%s", len(in.Text.HTML), "Process.Bandwidth", domain)
+	if l := len(in.Text.HTML); int64(l) == downloader.MaxFileSize {
+		logger.Error.Printf("%s Download failure: Document larger than max file size (%d)", prefix, l)
+		return
+	}
+	logger.Debug.Printf("%s Download success. %d bytes took %s", prefix, len(in.Text.HTML), time.Since(start))
 
 	// If any step fails along the way, save the article's state
 	defer func() {
 		if err = s.client.Call("StorageWriter.Article", in, in); err != nil {
-			logger.Error.Printf("Article.Process: [P:%s] [F:%s] [A:%s] [U:%s] Error saving: %s", in.PublicationId.Hex(), in.FeedId.Hex(), in.ID.Hex(), in.URL, err)
+			logger.Error.Printf("%s Error saving: %s", prefix, err)
 		}
 	}()
 
 	// Extract body
 	start = time.Now()
 	if err = body.SetBody(in); err != nil || in.Text.Body.Text == nil || len(in.Text.Body.Text) == 0 {
-		logger.Debug.Printf("%s %s.%s", time.Since(start), "Process.Body.Failure", domain)
-		logger.Error.Printf("%s[%s] Error setting body: %s", in.ID.Hex(), in.URL, err)
+		logger.Error.Printf("%s Body extraction error: %s", prefix, err)
 		return
 	}
-	logger.Debug.Printf("%s %s.%s", time.Since(start), "Process.Body.Success", domain)
-	logger.Debug.Printf("%d %s.%s", len(in.Text.Body.Text), "Process.BodyLength", domain)
 
 	// Filter out individual words
 	in.Text.Words.All = lexer.Words(in.Text.Body.Text)
-	logger.Debug.Printf("%d %s", len(in.Text.Words.All), "Process.Words")
 
 	// Filter out Keywords
 	in.Text.Words.Keywords = lexer.Keywords(in.Text.Body.Text)
-	logger.Debug.Printf("%d %s", len(in.Text.Words.Keywords), "Process.Keywords")
+
+	logger.Debug.Printf("%s Body Length: %d; Words: %d; Keywords: %d; Took: %s", len(in.Text.Body.Text), len(in.Text.Words.All), len(in.Text.Words.Keywords), time.Since(start))
 	return
 }
