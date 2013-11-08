0b6b68b0d9ac7678bd709c588b1de8fbb5e830e0
jake <bigjake2s@gmail.com>
2013-11-08 07:43:36 -0500

    Added Article stats (finally)

--- Article/main.go
+++ Article/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/300brand/coverage/article/lexer"
 	"github.com/300brand/coverage/downloader"
 	"github.com/300brand/coverageservices/service"
+	"github.com/300brand/coverageservices/types"
 	"github.com/300brand/disgo"
 	"github.com/300brand/logger"
 	"time"
@@ -35,27 +36,24 @@ func (s *Service) Process(in *coverage.Article, out *disgo.NullType) (err error)
 	start := time.Now()
 	prefix := fmt.Sprintf("Article.Process: [P:%s] [F:%s] [A:%s] [U:%s]", in.PublicationId.Hex(), in.FeedId.Hex(), in.ID.Hex(), in.URL)
 
-	// Used for stats - coming soon
-	// hostChunks := strings.Split(in.URL.Host, ".")
-	// if len(hostChunks) > 2 {
-	// 	hostChunks = hostChunks[len(hostChunks)-2:]
-	// }
-	// domain := strings.Join(hostChunks, "_")
-
 	// Download article
 	if err = downloader.Article(in); err != nil {
+		s.client.Call("Stats.Increment", &types.Stat{Name: "Article.Process.Errors.Download", Count: 1}, disgo.Null)
 		logger.Error.Printf("%s Download error: %s", prefix, err)
 		return
 	}
 	if l := len(in.Text.HTML); int64(l) == downloader.MaxFileSize {
-		logger.Error.Printf("%s Download failure: Document larger than max file size (%d)", prefix, l)
-		return fmt.Errorf("Document larger than max file size. URL: %s", in.URL)
+		s.client.Call("Stats.Increment", &types.Stat{Name: "Article.Process.Errors.DownloadTooBig", Count: 1}, disgo.Null)
+		err = fmt.Errorf("Document larger than max file size (%d)", downloader.MaxFileSize)
+		logger.Error.Printf("%s Download failure: %s", prefix, err)
+		return
 	}
 	logger.Debug.Printf("%s Download success. %d bytes took %s", prefix, len(in.Text.HTML), time.Since(start))
 
 	// If any step fails along the way, save the article's state
 	defer func() {
 		if err = s.client.Call("StorageWriter.Article", in, in); err != nil {
+			s.client.Call("Stats.Increment", &types.Stat{Name: "Article.Process.Errors.Database", Count: 1}, disgo.Null)
 			logger.Error.Printf("%s Error saving: %s", prefix, err)
 		}
 	}()
@@ -63,16 +61,21 @@ func (s *Service) Process(in *coverage.Article, out *disgo.NullType) (err error)
 	// Extract body
 	start = time.Now()
 	if err = body.SetBody(in); err != nil || in.Text.Body.Text == nil || len(in.Text.Body.Text) == 0 {
+		s.client.Call("Stats.Increment", &types.Stat{Name: "Article.Process.Errors.BodyExtraction", Count: 1}, disgo.Null)
 		logger.Error.Printf("%s Body extraction error: %s", prefix, err)
 		return
 	}
+	s.client.Call("Stats.Increment", &types.Stat{Name: "Article.Process.Body.Size", Count: len(in.Text.Body.Text)}, disgo.Null)
 
 	// Filter out individual words
 	in.Text.Words.All = lexer.Words(in.Text.Body.Text)
+	s.client.Call("Stats.Increment", &types.Stat{Name: "Article.Process.Body.Words", Count: len(in.Text.Words.All)}, disgo.Null)
 
 	// Filter out Keywords
 	in.Text.Words.Keywords = lexer.Keywords(in.Text.Body.Text)
+	s.client.Call("Stats.Increment", &types.Stat{Name: "Article.Process.Body.Keywords", Count: len(in.Text.Words.Keywords)}, disgo.Null)
 
+	s.client.Call("Stats.Duration", &types.Stat{Name: "Article.Process", Duration: time.Since(start)}, disgo.Null)
 	logger.Debug.Printf("%s Body Length: %d; Words: %d; Keywords: %d; Took: %s", prefix, len(in.Text.Body.Text), len(in.Text.Words.All), len(in.Text.Words.Keywords), time.Since(start))
 	return
 }
