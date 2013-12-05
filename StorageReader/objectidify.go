743d63d49a05fa8ee38d0ec82e03b111dd01889e
jake <bigjake2s@gmail.com>
2013-12-05 14:06:00 -0500

    Switched to reflection to handle all of the bson.ObjectId casting

--- StorageReader/objectidify.go
+++ StorageReader/objectidify.go
@@ -2,27 +2,61 @@ package StorageReader
 
 import (
 	"labix.org/v2/mgo/bson"
+	"reflect"
 )
 
-// Recursively scans a bson.M to convert any string values back into ObjectIds
-// because JSON loses the type during transport
-func objectIdify(m *bson.M) {
-	for k, v := range *m {
-		switch i := v.(type) {
-		case string:
-			if bson.IsObjectIdHex(i) {
-				(*m)[k] = bson.ObjectIdHex(i)
+// Recursively scans a query to convert any ObjectId string values back into
+// ObjectIds because JSON loses the type during transport
+func objectIdify(in interface{}) {
+	v := reflect.ValueOf(in)
+	switch v.Kind() {
+	case reflect.Ptr:
+		v = v.Elem()
+	default:
+		return
+	}
+
+	reflectInto(v)
+}
+
+func reflectInto(v reflect.Value) {
+	orig := v
+	// Spin out the map/slice stuff
+	for {
+		switch v.Kind() {
+		case reflect.Map:
+			// Maps keep coming in as map[...]interface{}, should be able to
+			// keep it this way until it fails
+			for _, key := range v.MapKeys() {
+				valInt := v.MapIndex(key).Elem().Interface()
+				valPtr := reflect.ValueOf(&valInt)
+				reflectInto(valPtr)
+				v.SetMapIndex(key, valPtr.Elem())
 			}
-		case []string:
-			ids := make([]bson.ObjectId, len(i))
-			for idx := range i {
-				if bson.IsObjectIdHex(i[idx]) {
-					ids[idx] = bson.ObjectIdHex(i[idx])
-				}
+			return
+		case reflect.Slice:
+			newSlice := make([]interface{}, v.Len())
+			for i := 0; i < v.Len(); i++ {
+				newSlice[i] = v.Index(i).Interface()
+				reflectInto(reflect.ValueOf(&newSlice[i]))
 			}
-			(*m)[k] = ids
-		case bson.M:
-			objectIdify(&i)
+			// Need the original pointer value to reset the slice to []interface{}
+			orig.Elem().Set(reflect.ValueOf(newSlice))
+			return
+		case reflect.Ptr:
+			// If it's a pointer, drop to the element pointed to
+			v = v.Elem()
+			// Test if the pointed element is a map or slice
+			newV := reflect.ValueOf(v.Interface())
+			if k := newV.Kind(); k == reflect.Slice || k == reflect.Map {
+				v = newV
+				continue
+			}
 		}
+		break
+	}
+
+	if s, ok := v.Interface().(string); ok && bson.IsObjectIdHex(s) {
+		v.Set(reflect.ValueOf(bson.ObjectIdHex(s)))
 	}
 }
--- StorageReader/objectidify_test.go
+++ StorageReader/objectidify_test.go
@@ -48,9 +48,62 @@ func TestIdArr(t *testing.T) {
 	switch v := m["_id"].(type) {
 	case bson.M:
 		switch w := v["$in"].(type) {
-		case []bson.ObjectId:
+		case []interface{}:
 			for i, id := range w {
-				if ids[i].String() != id.String() {
+				bId, ok := id.(bson.ObjectId)
+				if !ok {
+					t.Errorf("Invalid id type [%T]: %v", id, id)
+					continue
+				}
+				if ids[i].String() != bId.String() {
+					t.Errorf("IDs do not match %s != %s", ids[i], id)
+				}
+			}
+		default:
+			t.Errorf("Invalid $in type [%T]: %+v", w, w)
+		}
+	default:
+		t.Errorf("Invalid _id type [%T]: %+v", v, v)
+	}
+}
+
+func TestMapInterface(t *testing.T) {
+	id := bson.NewObjectId()
+	m := bson.M{"_id": map[string]interface{}{"$not": id.Hex()}}
+	objectIdify(&m)
+	switch v := m["_id"].(type) {
+	case map[string]interface{}:
+		switch w := v["$not"].(type) {
+		case bson.ObjectId:
+			if id.String() != w.String() {
+				t.Errorf("IDs do not match %s != %s", id, w)
+			}
+		default:
+			t.Errorf("Invalid $not type [%T]: %+v", w, w)
+		}
+	default:
+		t.Errorf("Invalid _id type [%T]: %+v", v, v)
+	}
+}
+
+func TestMapInterfaceArr(t *testing.T) {
+	ids := []bson.ObjectId{
+		bson.NewObjectId(),
+		bson.NewObjectId(),
+	}
+	m := bson.M{"_id": map[string]interface{}{"$in": []interface{}{ids[0].Hex(), ids[1].Hex()}}}
+	objectIdify(&m)
+	switch v := m["_id"].(type) {
+	case map[string]interface{}:
+		switch w := v["$in"].(type) {
+		case []interface{}:
+			for i, id := range w {
+				bId, ok := id.(bson.ObjectId)
+				if !ok {
+					t.Errorf("Invalid id type [%T]: %v", id, id)
+					continue
+				}
+				if ids[i].String() != bId.String() {
 					t.Errorf("IDs do not match %s != %s", ids[i], id)
 				}
 			}
