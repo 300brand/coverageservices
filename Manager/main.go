14c66dc02985279df03d83c7992c45f053c022a9
jake <bigjake2s@gmail.com>
2014-01-14 16:50:47 -0500

    Updated config layout and added article ticker bits

--- Manager/main.go
+++ Manager/main.go
@@ -1,6 +1,7 @@
 package Manager
 
 import (
+	"github.com/300brand/coverage"
 	"github.com/300brand/coverageservices/service"
 	"github.com/300brand/coverageservices/types"
 	"github.com/300brand/disgo"
@@ -16,11 +17,13 @@ type Service struct {
 var (
 	_ service.Service = &Service{}
 
-	cfgStartup       = config.Bool("Manager.startup", false)
-	cfgTick          = config.Duration("Manager.tick", 10*time.Second)
-	cfgDownloadDelay = config.Duration("Manager.downloaddelay", -2*time.Hour)
+	cfgStartup           = config.Bool("Manager.startup", false)
+	cfgArticleTick       = config.Duration("Manager.article.tick", 10*time.Second)
+	cfgFeedTick          = config.Duration("Manager.feed.tick", 10*time.Second)
+	cfgFeedDownloadDelay = config.Duration("Manager.feed.downloaddelay", -2*time.Hour)
 
-	tProcessor *Ticker
+	aProcessor *Ticker
+	fProcessor *Ticker
 )
 
 func init() {
@@ -32,24 +35,43 @@ func init() {
 func (s *Service) Start(client *disgo.Client) (err error) {
 	s.client = client
 
-	tProcessor = NewTicker(s.processFeed, *cfgTick)
-	go tProcessor.Run()
+	aProcessor = NewTicker(s.processArticle, *cfgArticleTick)
+	go aProcessor.Run()
+
+	fProcessor = NewTicker(s.processFeed, *cfgFeedTick)
+	go fProcessor.Run()
 
 	if *cfgStartup {
-		tProcessor.Start <- true
+		aProcessor.Start <- true
+		fProcessor.Start <- true
 	}
 	return
 }
 
 // Service funcs
 
+func (s *Service) ArticleProcessor(in *types.ClockCommand, out *disgo.NullType) (err error) {
+	return aProcessor.ProcessCommand(in)
+}
+
 func (s *Service) FeedProcessor(in *types.ClockCommand, out *disgo.NullType) (err error) {
-	return tProcessor.ProcessCommand(in)
+	return fProcessor.ProcessCommand(in)
+}
+
+func (s *Service) processArticle() (err error) {
+	logger.Debug.Printf("processArticle: Getting Article")
+	a := new(coverage.Article)
+	if err = s.client.Call("StorageWriter.ArticleQueueNext", disgo.Null, a); err != nil {
+		logger.Warn.Printf("No article to process right now: %s", err)
+		return
+	}
+	logger.Debug.Printf("processArticle: Got %s", a.ID.Hex())
+	return s.client.Call("Article.Process", a, disgo.Null)
 }
 
 func (s *Service) processFeed() (err error) {
 	id := new(types.ObjectId)
-	thresh := types.DateThreshold{time.Now().Add(*cfgDownloadDelay)}
+	thresh := types.DateThreshold{time.Now().Add(*cfgFeedDownloadDelay)}
 	logger.Debug.Printf("processFeed: Getting ID")
 	if err = s.client.Call("StorageWriter.NextDownloadFeedId", thresh, id); err != nil {
 		logger.Error.Printf("Manager.processFeed: %s", err)
