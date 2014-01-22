ff636bef9addc42b11133a224e70909361c483d1
jake <bigjake2s@gmail.com>
2014-01-22 11:09:59 -0500

    Adding the initial GroupSearch brains

--- Search/group_search.go
+++ Search/group_search.go
@@ -3,14 +3,48 @@ package Search
 import (
 	"github.com/300brand/coverage"
 	"github.com/300brand/coverageservices/types"
+	"github.com/300brand/disgo"
+	"github.com/300brand/logger"
 	"labix.org/v2/mgo/bson"
+	"sync"
+	"time"
 )
 
 func (s *Service) GroupSearch(in *types.GroupQuery, out *types.SearchQueryResponse) (err error) {
-	gs := coverage.GroupSearch{
+	gs := &coverage.GroupSearch{
 		Id: bson.NewObjectId(),
 	}
 	s.client.Call("StorageWriter.NewGroupSearch", gs, gs)
+
+	searchQuery := types.SearchQuery{
+		Dates:          in.Dates,
+		PublicationIds: in.PublicationIds,
+		Foreground:     true,
+	}
+	// Do not want the complete notification to send out after each sub-search
+	searchQuery.Notify.Social = in.Notify.Social
+
+	var wg sync.WaitGroup
+	for _, q := range in.Queries {
+		wg.Add(1)
+		searchQuery.Q = q.Q
+		go func(sq types.SearchQuery) {
+			s.client.Call("Search.Search", sq, disgo.Null)
+			wg.Done()
+		}(searchQuery)
+	}
+
+	// Wait for all of the DateSearch calls to finish, then send the
+	// notification of completeness
+	go func(gs *coverage.GroupSearch) {
+		wg.Wait()
+		if gs.Notify.Done != "" {
+			if err := s.client.Call("Search.NotifyComplete", types.ObjectId{gs.Id}, disgo.Null); err != nil {
+				logger.Error.Print(err)
+			}
+		}
+		logger.Info.Printf("Group Search completed in %s", time.Since(gs.Start))
+	}(gs)
 
 	// Prepare information for the caller
 	out.Id = gs.Id
